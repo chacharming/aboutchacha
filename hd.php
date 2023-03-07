8d43767fb4b59fc6ca5fd59476a79efa7ea66d68
chacharming <ckwanl0723@naver.com>
2023-03-07 10:57:48 +0900

    php를 이용하여 해드 연결 및 접근성을 고려하여 태그 다시 짜기

---
+++ hd.php
@@ -0,0 +1,32 @@
+<header id="hd" class="d-flex flex-column">
+    <h1><a href="" class="sr-only">차주미 자기소개페이지 입니다.</a></h1>
+    <div class="d-flex flex-column justify-content-between">
+        <ul id="hdTop" class="d-flex">
+            <li class="col-5 px-0">
+                <ul class="d-flex flex-column pt-5 folderBox">
+                    <li class="mb-5 major"><a href="" class="d-inline-block pt-2">major</a></li>
+                    <li class="minor"><a href="" class="d-inline-block pt-2">minor</a></li>
+                </ul>
+            </li>
+            <li class="col-7 px-0 pt-3 d-flex flex-column flex-wrap">
+                <div class="power"></div>
+                <div class="d-flex flex-column">
+                    <ul class="d-flex py-5 pagenation">
+                        <li class="col-6 px-0 clickPage"><a href="" title="인덱스페이지입니다"></a></li>
+                        <li class="col-6 px-0 noneClick"><a href="" title="포트폴리오페이지입니다"></a></li>
+                    </ul>
+                    <ul class="d-flex pt-2 text-center iconBox">
+                        <li class="col-6 px-0 note"><a href="" class="d-inline-block pt-2">contect</a></li>
+                        <li class="col-6 px-0 memo"><a href="" class="d-inline-block pt-2">qna</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        <div id="hdBottom" class="position-relative">
+            <div class="heart"></div>
+            <div class="position-absolute">
+                <div class="selfi0"></div>
+            </div>
+        </div>
+    </div>    
+</header>
